d1f805e08eda2577365ac297125214ad4f4950f9
Marco Oesch <marco.oesch@decentage.com>
2021-05-06 17:20:22 +0200

    updated docker build script

--- build-docker.sh
+++ build-docker.sh
@@ -4,4 +4,4 @@ set -o pipefail
 set -o nounset
 
 RELEASE_VERSION=$(cat release-version.txt)
-docker build --build-arg RELEASE_VERSION=${RELEASE_VERSION} -t decentage/iep-node:latest .
+docker build -t decentage/iep-node:latest .
