3f48eef75f21e83c949c257401a128c646a71ed6
Marco Oesch <marco.oesch@decentage.com>
2021-05-27 14:19:34 +0200

    removed docker container after build

--- build-docker.sh
+++ build-docker.sh
@@ -6,6 +6,7 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-node:${RELEASE_VERSION} .
 
-CID=$(docker create decentage/iep-node:${RELEASE_VERSION})
+CONTAINER_ID=$(docker create --rm --name iep-node-extract decentage/iep-node:${RELEASE_VERSION})
 mkdir -p ./build
-docker cp ${CID}:/iep-node.zip ./build
+docker cp ${CONTAINER_ID}:/iep-node.zip ./build
+docker rm ${CONTAINER_ID}
