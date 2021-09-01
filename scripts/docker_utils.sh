07d668a97eb36f5b0ebf3c2b10a22b438b2f45ba
Marco Oesch <marco.oesch@decentage.com>
2021-09-01 12:06:10 +0200

    updated init secret script

--- scripts/docker_utils.sh
+++ scripts/docker_utils.sh
@@ -6,7 +6,13 @@ init_secret() {
 	if [[ -f "/run/secrets/${secret_name}" ]]; then
 		echo "Initializing secret ${secret_name} from secret /run/secrets/${secret_name}"
 		local secret_value=$(cat /run/secrets/${secret_name})
-		export ${secret_name}="${secret_value}"
+		
+		if [[ -z "${secret_value}" ]]; then
+			echo "The provided secret in /run/secrets/${secret_name} is empty. Therfore ignoring secret file."
+		else
+			export ${secret_name}="${secret_value}"
+		fi		
+
 	elif [[ -n ${secret_name:-} ]]; then
 		echo "Initializing secret ${secret_name} from variable ${secret_name}"
 	else
@@ -19,9 +25,7 @@ init_base64_secret() {
 	local secret_name="$1"
 	local secret_base64_name=${secret_name}_BASE64
 	init_secret $secret_base64_name
-	if [[ -z "${secret_base64_name}" ]]; then
-		echo "The provided secret in /run/secrets/${secret_name} is empty. Therfore ignoring secret file."
-	else
+	if [[ ! -z "${secret_base64_name}" ]]; then
 		export ${secret_name}="$(echo ${!secret_base64_name} | base64 -d)"
 		unset secret_base64_name
 	fi
