bc9962f723580aa1e136e29617ecac5316fb4b27
mazila <mazila@mazila-xps159560>
2021-10-26 15:10:57 +0200

    update

---
+++ src/test/java/xin/http/SendMessageTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2013-2016 The Nxt Core Developers.
+ * Copyright © 2016-2020 Jelurida IP B.V.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,
+ * no part of the Nxt software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package xin.http;
+
+import xin.Account;
+import xin.BlockchainTest;
+import xin.Constants;
+import xin.crypto.Crypto;
+import xin.crypto.EncryptedData;
+import xin.http.nxt.APICall;
+import xin.util.Convert;
+import xin.util.Logger;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SendMessageTest extends BlockchainTest {
+
+    @Test
+    public void sendMessage() {
+    	System.out.println("Hello first test");
+        JSONObject response = new APICall.Builder("sendMessage").
+                param("secretPhrase", ALICE.getSecretPhrase()).
+                param("recipient", BOB.getStrId()).
+                param("message", "hello world").
+                param("feeTQT", Constants.ONE_XIN).
+                build().invoke();
+        
+        System.out.println("Hello first test *************************** ");
+       
+        String transaction = (String) response.get("transaction");
+        
+        JSONObject attachment = (JSONObject) ((JSONObject)response.get("transactionJSON")).get("attachment");
+        Assert.assertEquals("hello world", attachment.get("message"));
+        
+        generateBlock();
+        
+        response = new APICall.Builder("readMessage").
+                param("secretPhrase", BOB.getSecretPhrase()).
+                param("transaction", transaction).
+                build().invoke();
+        Logger.logDebugMessage("readMessage: " + response);
+        Assert.assertEquals("hello world", response.get("message"));
+    }
+
+    
+}
---
+++ src/test/java/xin/http/SendMoneyTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2013-2016 The Nxt Core Developers.
+ * Copyright © 2016-2020 Jelurida IP B.V.
+ *
+ * See the LICENSE.txt file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,
+ * no part of the Nxt software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE.txt file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+package xin.http;
+
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import xin.BlockchainTest;
+import xin.Constants;
+import xin.http.nxt.APICall;
+import xin.util.Logger;
+
+public class SendMoneyTest extends BlockchainTest {
+
+    @Test
+    public void sendMoney() {
+        JSONObject response = new APICall.Builder("sendMoney").
+                param("secretPhrase", ALICE.getSecretPhrase()).
+                param("recipient", BOB.getStrId()).
+                param("amountTQT", 100 * Constants.ONE_XIN).
+                param("feeTQT", Constants.ONE_XIN).
+                build().invokeNoError();
+      
+        Logger.logDebugMessage("sendMoney: " + response);
+        // Forger
+        Assert.assertEquals(0, FORGY.getBalanceDiff());
+        Assert.assertEquals(0, FORGY.getUnconfirmedBalanceDiff());
+        // Sender
+        Assert.assertEquals(0, ALICE.getBalanceDiff());
+        Assert.assertEquals(-100 * Constants.ONE_XIN - Constants.ONE_XIN, ALICE.getUnconfirmedBalanceDiff());
+        // Recipient
+        Assert.assertEquals(0, BOB.getBalanceDiff());
+        Assert.assertEquals(0, BOB.getUnconfirmedBalanceDiff());
+        
+        
+        	generateBlock();
+        	generateBlock();
+        
+        
+        
+        // Forger
+        Assert.assertEquals(Constants.ONE_XIN, FORGY.getBalanceDiff());
+        Assert.assertEquals(Constants.ONE_XIN, FORGY.getUnconfirmedBalanceDiff());
+        // Sender
+        Assert.assertEquals(-100 * Constants.ONE_XIN - Constants.ONE_XIN, ALICE.getBalanceDiff());
+        Assert.assertEquals(-100 * Constants.ONE_XIN - Constants.ONE_XIN, ALICE.getUnconfirmedBalanceDiff());
+        // Recipient
+        Assert.assertEquals(100 * Constants.ONE_XIN, BOB.getBalanceDiff());
+        Assert.assertEquals(100 * Constants.ONE_XIN, BOB.getUnconfirmedBalanceDiff());
+    }
+
+   
+}
